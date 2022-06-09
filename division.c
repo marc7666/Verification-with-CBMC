d7b0646478f9753021fc518ec678fc9b0573cdbd
Marc Cervera <64266158+marc7666@users.noreply.github.com>
2022-06-09 17:55:17 +0200

    Update division.c

--- division.c
+++ division.c
@@ -3,8 +3,7 @@
 int main( void ) {
   int D,d, r, q;
 
-  __CPROVER_assume( (D < 0) &&  (D >= -300) &&
-                   (d >= 0)  );
+  __CPROVER_assume( (D < 0) &&  (D >= -300) && (d > 0) );
   r = D; 
   q = 0;
   while ( r < 0 ) {
@@ -12,5 +11,6 @@ int main( void ) {
      q--;   
   }
   printf( " quotient: %d,  reminder: %d\n", q, r );
+  __CPROVER_assert((q == D/d), "q is the quotient");
+  __CPROVER_assert((r == d%d), "r is the reminder");
 }
-
